6b4a5eec01934e9e386ff9ffaeed12f5dc2531af
Christoph Pakr <c.pakr@me.com>
2024-06-14 09:58:54 +0200

    Refactor socket handler for efficiency and client connection handling
    The socket handler has been refactored to improve the efficiency and handling of client connections. This includes modifications on how the socket opens and closes, along with the introduction of a "_client_connected" flag to manage the connection status. Furthermore, the method to send images has been updated to now concatenate multiple images before sending.

--- socket_handler.py
+++ socket_handler.py
@@ -1,15 +1,19 @@
 import socket, pickle, struct
 import threading
 import time
-import imutils
-from turbojpeg import TurboJPEG
+# from turbojpeg import TurboJPEG
 import platform
+import numpy as np
+
+# if platform.system() == "Linux":
+#     # self._log_info_vsensor("set parameters for linux-system")
+#     jpeg = TurboJPEG()
+#     showOutput = False
 
 
-if platform.system() == "Linux":
-    # self._log_info_vsensor("set parameters for linux-system")
-    jpeg = TurboJPEG()
-    showOutput = False
+
+
+
 
 class ActiveSocketConnections:
     def __init__(self):
@@ -30,22 +34,18 @@ class SocketHandler:
         self.host_port = port
         self.max_listeners = max_listeners
         self.active_connections = ActiveSocketConnections()
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socketOpen()
+        # self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._t_image = None
+        self._client_connected = False
         self._start_transfer = False
         self.s_thread = threading.Thread(target=self._bg_task)
         self.s_thread.daemon = True
         self.s_thread.start()
 
     def _bg_task(self):
+        self.socketOpen()
         while True:
             try:
-                # self.client_socket, addr = self.server_socket.accept()
-                # if self.client_socket:
-                #     self._log_info("Got a connection from {}".format(str(addr)))
-                #     self.active_connections.register_new_connection(addr)
-                # print(self._start_transfer)
                 if self._start_transfer:
                     # print("transfer image")
                     if self._t_image is not None:
@@ -53,33 +53,38 @@ class SocketHandler:
                         a = pickle.dumps(self._t_image)
                         message = struct.pack("Q", len(a)) + a
                         self.conn.sendall(message)
-                        # print("sendframe with size: ", len(a), len(image_info_jpg), time.time() - start)
+                        print("sendframe with size: ", len(a), len(self._t_image), time.time() - start)
                         self._start_transfer = False
                         self._t_image = None
-                        time.sleep(0.05)
+                        time.sleep(0.04)
 
             except Exception as e:
                 self._log_info(e)
+                self._client_connected = False
                 self.socketClose()
-                self.socketOpen()
+                time.sleep(1)
+                # self.socketOpen()
                 self.s_thread = threading.Thread(target=self._bg_task)
                 self.s_thread.start()
+                break
 
     def socketOpen(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host_ip, self.host_port))
-        self.server_socket.listen(self.max_listeners)
+        self.server_socket.listen(1)
         self._log_info(u'Server socket [ TCP_IP: ' + self.host_ip + ', TCP_PORT: ' + str(self.host_port) + ' ] is open')
         self.conn, self.addr = self.server_socket.accept()
-        self.active_connections.register_new_connection(self.addr)
+        print("##########################################################################")
+        self._client_connected = True
         self._log_info(u'Server socket [ TCP_IP: ' + self.host_ip + ', TCP_PORT: ' + str(self.host_port) + ' ] is connected with client')
 
     def socketClose(self):
         self.server_socket.close()
         self._log_info(u'Server socket [ TCP_IP: ' + self.host_ip + ', TCP_PORT: ' + str(self.host_port) + ' ] is close')
 
-    def send_image(self, image):
-        self._t_image = image
+    def send_image(self, image_rear, image_front):
+        self._t_image = np.concatenate((image_rear, image_front), axis=1)
         self._start_transfer = True
 
     def _log_info(self, message):
